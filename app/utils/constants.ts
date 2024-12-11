67f763806de5c7626f4009b425e9f31ddaa7b044
Francesco Sardo <francesco.sardo@shopify.com>
2024-12-11 17:25:14 +0000

    Default to gemini

--- app/utils/constants.ts
+++ app/utils/constants.ts
@@ -7,7 +7,7 @@ export const WORK_DIR = `/home/${WORK_DIR_NAME}`;
 export const MODIFICATIONS_TAG_NAME = 'bolt_file_modifications';
 export const MODEL_REGEX = /^\[Model: (.*?)\]\n\n/;
 export const PROVIDER_REGEX = /\[Provider: (.*?)\]\n\n/;
-export const DEFAULT_MODEL = 'anthropic:claude-3-5-sonnet-v2@20241022';
+export const DEFAULT_MODEL = 'google:gemini-1.5-pro-001'; // google:gemini-1.5-pro-001 // openai:gpt-4o-2024-08-06 // anthropic:claude-3-5-sonnet-v2@20241022
 export const PROMPT_COOKIE_KEY = 'cachedPrompt';
 
 const PROVIDER_LIST: ProviderInfo[] = [
