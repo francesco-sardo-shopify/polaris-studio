1340916ca025f5b0efed948946b40543d03627f9
Natalie Dodd <natalie.dodd@shopify.com>
2024-12-13 14:38:05 +0000

    feat: add icon to landing page

--- app/components/sidebar/Menu.client.tsx
+++ app/components/sidebar/Menu.client.tsx
@@ -2,7 +2,7 @@ import { motion, type Variants } from 'framer-motion';
 import { useCallback, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { Dialog, DialogButton, DialogDescription, DialogRoot, DialogTitle } from '~/components/ui/Dialog';
-import { ThemeSwitch } from '~/components/ui/ThemeSwitch';
+import { PolarisStudioIcon } from '~/components/ui/PolarisStudioIcon';
 import { Settings } from '~/components/ui/Settings';
 import { SettingsButton } from '~/components/ui/SettingsButton';
 import { db, deleteById, getAll, chatId, type ChatHistoryItem, useChatHistory } from '~/lib/persistence';
@@ -128,17 +128,7 @@ export const Menu = () => {
     >
       <div className="flex items-center h-[var(--header-height)] pl-6 pr-5 pt-6 my-2">
         <div className="w-5 h-5 mr-2">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path
-              fill="#62c139"
-              d="M5.702 4.253a.625.625 0 0 1 1.096 0l.196.358c.207.378.517.688.895.895l.358.196a.625.625 0 0 1 0 1.097l-.358.196a2.25 2.25 0 0 0-.895.894l-.196.359a.625.625 0 0 1-1.096 0l-.196-.359a2.25 2.25 0 0 0-.895-.894l-.358-.196a.625.625 0 0 1 0-1.097l.358-.196a2.25 2.25 0 0 0 .895-.895l.196-.358Z"
-            />
-            <path
-              fill="#62c139"
-              fill-rule="evenodd"
-              d="M12.948 7.89c-.18-1.167-1.852-1.19-2.064-.029l-.03.164a3.756 3.756 0 0 1-3.088 3.031c-1.15.189-1.173 1.833-.03 2.054l.105.02a3.824 3.824 0 0 1 3.029 3.029l.032.165c.233 1.208 1.963 1.208 2.196 0l.025-.129a3.836 3.836 0 0 1 3.077-3.045c1.184-.216 1.12-1.928-.071-2.107a3.789 3.789 0 0 1-3.18-3.154Zm-.944 6.887a5.34 5.34 0 0 1 2.542-2.647 5.305 5.305 0 0 1-2.628-2.548 5.262 5.262 0 0 1-2.488 2.508 5.329 5.329 0 0 1 2.574 2.687Z"
-            />
-          </svg>
+          <PolarisStudioIcon color="#62c139" />
         </div>
         <p className="text-bolt-elements-textPrimary text-xl font-bold ">Polaris Studio</p>
       </div>
